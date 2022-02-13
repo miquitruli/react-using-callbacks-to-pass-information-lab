2d7e1e535b1782e9d167d0ff51743de81fb78942
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-12 19:09:48 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import learnSymbol from './data.js'
-import Cell from './Cell.js'
-import ColorSelector from './ColorSelector.js'
+import Cell from './Cell.js' //
+import ColorSelector from './ColorSelector.js' //is the interface where users will be able to select a color
 
 export default class Matrix extends Component {
-
+  //initial state, white
   constructor() {
     super()
+    this.state = {
+      selectedColor: '#FFF'
+    }
   }
 
+  //increment, this method is to change the default value
+  //this method updates selectedColor with whatever is passed into it as an argument.
+  setSelectedColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+
+  //Cell, on the other hand, only needs to know the currently selected color, not change it. 
+  //We can pass this in to every Cell returned by genRow() as a prop called selectedColor:
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
   )
 
   genMatrix = () => (
@@ -20,7 +33,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
